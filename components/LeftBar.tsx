c6b512b08238b1454e00e1b134e0a7d3b216aa7b
chege4179 <peterkagure@gmail.com>
2022-09-23 13:43:52 +0300

    fixed redirect url

--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useSession, signOut} from "next-auth/react";
 import Image from "next/image";
+import BASE_URL from "../util/BaseURL";
 
 const LeftBar = () => {
      const { data: session } = useSession()
@@ -8,7 +9,7 @@ const LeftBar = () => {
      return (
           <div className="w-3/12 px-1 mr-4  greyish-color rounded-2xl drop-shadow-lg sm:w-full sm:p-0 md:w-full md:p-0 sm:mt-12">
                <div className="flex mx-4 py-2 ">
-                    <div className='w-12 h-12 relative' onClick={() => signOut({ callbackUrl: 'http://localhost:3000' })}>
+                    <div className='w-12 h-12 relative' onClick={() => signOut({ callbackUrl: BASE_URL })}>
                          <Image src={session?.user?.image!} layout='fill' className='rounded-3xl cursor-pointer' alt={session?.user?.name!} />
                     </div>
                     <div className="ml-4">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type {NextPage} from 'next'
 import {signIn} from "next-auth/react";
+import BASE_URL from "../util/BaseURL";
 
 
 const Home: NextPage = () => {
@@ -8,7 +9,7 @@ const Home: NextPage = () => {
           <div className="w-screen h-screen bg-black flex items-center justify-center">
                <button
                     className="btn-primary"
-                    onClick={() => signIn("google",{ callbackUrl:"http://localhost:3000/home" })}
+                    onClick={() => signIn("google",{ callbackUrl:`${BASE_URL}/home` })}
                >
                     Login With Google
                </button>
